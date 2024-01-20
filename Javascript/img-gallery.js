9f8360bc0909f3a6a64a54191c76436a567c537f
Rohan <66969681+RohanDinesh@users.noreply.github.com>
2024-01-20 22:01:06 +0530

    website

---
+++ Javascript/img-gallery.js
@@ -0,0 +1,12 @@
+// window.functionName= functionName;
+var fullImgBox = document.getElementById("fullImgBox");
+var fullImg = document.getElementById("fullImg");
+
+function openFullImg(pic){
+  fullImgBox.style.display = 'flex';
+  fullImg.src=pic;
+}
+
+function closeFullImg(pic){
+  fullImgBox.style.display = 'none';
+}
